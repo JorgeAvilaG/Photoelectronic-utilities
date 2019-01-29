fa390dd1214162f38a65bbb28e4a955ba4bff04e
Jorge Avila <46384243+JorgeAvilaG@users.noreply.github.com>
2019-01-29 13:01:23 +0100

    Add files via upload

---
+++ CalcularMasa.py
@@ -0,0 +1,62 @@
+"""
+Programa que calcula la cantidad de disolvente y de ionic liquid que necesitas para
+preparar la disolución de complejo
+
+"""
+
+
+print(' Ionic Liquid used BMIM:PF6') #Muestra los datos del IL que usamos siempre
+print('MolecularMass = 284.18 g/mol')
+print('Solution 13 mg/ml acetonitrile')
+
+PesoMolecularIL = 284.18           # los datos del IL que usamos siempre
+ConcentracionDisolucionIL = 13
+
+CambiarIL = input('Is correct IL dates?(y/n): ') #Pregunta para cambiar los datos de IL si quieres utilizar una diferente
+if CambiarIL == "n":
+    PesoMolecularIL = float(input('Insert Molecular Mass of the new IL(g/mol): '))
+    ConcentracionDisolucionIL = float(input('Insert concentration of the IL solution(mg/ml): '))
+
+print('   ')#Imprime los datos finales de IL
+print('IL dates:')
+print('MolecularMass =',PesoMolecularIL,'g/mol')
+print('Solution concentration =',ConcentracionDisolucionIL,'mg/ml')
+print('   ')
+
+
+PesoMolecularComplejo = float(input('Insert molecular mass of the complex: ')) #Pide la masamolecular del complejo a usar
+ConcentracionComplejo = 20    #Supone que 20 sera la concentracion de complejo
+CambiarConcentracion = input('The complex solution concentration is 20 mg/ml?(y/n): ')#Pregunta si quieres usar una concentracion diferente
+if CambiarConcentracion == 'n':
+    ConcentracionComplejo = float(input('Insert complex solution concentration: '))
+  
+print('   ')    #Imprime los datos del complejo
+print('Complex dates:')
+print('MolecularMass =',PesoMolecularComplejo,'g/mol')
+print('Solution concentration =',ConcentracionComplejo,'mg/ml')
+print('   ')
+
+
+RatioCIL = float(input('Insert Ratio Complex:IL (Ex for 4:1 insert 4): ')) #Calcula los milimoles de complejo e IL
+MMolesMlComplejo = ConcentracionComplejo/PesoMolecularComplejo
+MMolesMlIL = MMolesMlComplejo/RatioCIL
+
+print('   ')                 
+#print('milimoles complex/ml =',MMolesMlComplejo,'mmol/ml')
+#print('milimoles IL/ml =',MMolesMlIL,'mmol/ml')
+#print('   ')
+
+MasaComplejo = float(input('Insert amount Complex weight (mg): '))
+TotalDisolucion = MasaComplejo/ConcentracionComplejo
+MMolesIL = TotalDisolucion*MMolesMlIL
+MgramosIL = MMolesIL*PesoMolecularIL
+MlIL = MgramosIL/ConcentracionDisolucionIL
+VolumenFalta = TotalDisolucion - MlIL
+
+
+print('Total disolution =',TotalDisolucion,'ml')
+print('Quantity of IL = ',MlIL,'ml (',MlIL*1000,'ul)')
+print('Add',VolumenFalta,'ml Acetonitrile (',VolumenFalta*1000,'ul)')
+
+
+
---
+++ zData.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 18 11:44:23 2019
+
+@author: Jorge A
+"""
+import os
+
+
+DirectorioActual = os.getcwd()
+ListaArchivos = os.listdir(DirectorioActual)   
+outfile = open(DirectorioActual+'\\'+'PCEs.txt', 'w')
+outfile.write("Time,Name,Cell,JSC (mA/cm2),VOC (V),FF (%),Maxim Power Point x cm2(mW/cm2),JSC CORRECTED (mA/cm2),Efficiency (%)\n")
+outfile.close()
+outfile = open(DirectorioActual+'\\'+'EQEs.txt', 'w')
+outfile.write("Time,Name,Cell,Photocurrent (mA/cm2)\n")
+outfile.close()
+
+
+
+
+def readfiles(file,name):
+    
+    with open(file) as in_file, open(DirectorioActual+'\\'+'EQEs.txt', 'a') as EQE_file, open(DirectorioActual+'\\'+'PCEs.txt', 'a') as PCE_file:
+       
+       data = [temp.strip() for temp in in_file.readlines()[0:30]]
+       
+       date = data[0]
+       
+       if data[3] == 'Real Photocurrent (mA/cm2)': #EQE simulador cleanroom
+            Photocurrents = data[4].split('\t')
+            Photocurrents = [str(round(float(temp),2)) for temp in Photocurrents]
+            Cell = 1
+            for data in Photocurrents:
+                EQE_file.write(date + ',' + name + ',' + str(Cell) + ',' + data + '\n')
+                Cell += 1
+                
+       elif data[4] == 'Maxim Power Point x cm2(mW/cm2)': #IPCE simulador cleanroom
+           MPP = data[5].split('\t')
+           Voc = data[8].split('\t')
+           Jsc = data[11].split('\t')
+           FF = data[14].split('\t')
+           Jscc = data[17].split('\t')
+           EPCE = data[20].split('\t')
+           
+           Voc = [str(round(float(temp)/1000,3)) for temp in Voc]
+           FF = [str(round(float(temp),1)) for temp in FF]
+           EPCE = [str(round(float(temp),2)) for temp in EPCE]           
+           MPP = [str(round(float(temp),2)) for temp in MPP]
+           Jsc = [str(round(float(temp),2)) for temp in Jsc]
+           Jscc = [str(round(float(temp),2)) for temp in Jscc]
+           
+           for cell in range(4):
+               PCE_file.write(date + ',' + name + ','+ str(cell+1) + ',' + Jsc[cell] + ',' + Voc[cell] + ',' + FF[cell] + ',' + MPP[cell] + ',' + Jscc[cell] + ',' + EPCE[cell] + '\n')
+       
+       elif data[11] == 'Go:': #SS LED
+           MPP = [data[16].split('=')[1]]
+           Voc = [data[13].split('=')[1]]
+           Jsc = [data[14].split('=')[1]]
+           FF = [data[15].split('=')[1]]
+           EPCE = [data[12].split('=')[1]]
+           Area = [data[19].split('=')[1]]
+           
+           
+           MPP.append(data[26].split('=')[1])
+           Voc.append(data[23].split('=')[1])
+           Jsc.append(data[24].split('=')[1])
+           FF.append(data[25].split('=')[1])
+           EPCE.append(data[22].split('=')[1])
+           
+           Jsc = [str(round(float(temp)/float(Area[0]),2)) for temp in Jsc]
+           MPP = [str(round(float(temp)/float(Area[0]),2)) for temp in MPP]
+           Voc = [str(round(float(temp),3)) for temp in Voc]
+           FF = [str(round(float(temp),1)) for temp in FF]
+           EPCE = [str(round(float(temp),2)) for temp in EPCE]
+           
+           
+           PCE_file.write(date + ',' + name +'_forward' + ','+ 'NaN' + ',' + Jsc[0] + ',' + Voc[0] + ',' + FF[0] + ',' + MPP[0] + ',' + 'NaN' + ',' + EPCE[0] + '\n')
+           PCE_file.write(date + ',' + name +'_backward' + ','+ 'NaN' + ',' + Jsc[1] + ',' + Voc[1] + ',' + FF[1] + ',' + MPP[1] + ',' + 'NaN' + ',' + EPCE[1] + '\n')    
+ 
+       elif data[8] == 'Go:': #SS Xenon lamp
+           MPP = [data[13].split('=')[1]]
+           Voc = [data[10].split('=')[1]]
+           Jsc = [data[11].split('=')[1]]
+           FF = [data[12].split('=')[1]]
+           EPCE = [data[9].split('=')[1]]
+           Area = [data[16].split('=')[1]]
+           
+           
+           MPP.append(data[23].split('=')[1])
+           Voc.append(data[20].split('=')[1])
+           Jsc.append(data[21].split('=')[1])
+           FF.append(data[22].split('=')[1])
+           EPCE.append(data[19].split('=')[1])
+           
+           Jsc = [str(round(float(temp)/float(Area[0]),2)) for temp in Jsc]
+           MPP = [str(round(float(temp)/float(Area[0]),2)) for temp in MPP]
+           Voc = [str(round(float(temp),3)) for temp in Voc]
+           FF = [str(round(float(temp),1)) for temp in FF]
+           EPCE = [str(round(float(temp),2)) for temp in EPCE]
+           
+           
+           PCE_file.write(date + ',' + name +'_forward' + ','+ 'NaN' + ',' + Jsc[0] + ',' + Voc[0] + ',' + FF[0] + ',' + MPP[0] + ',' + 'NaN' + ',' + EPCE[0] + '\n')
+           PCE_file.write(date + ',' + name +'_backward' + ','+ 'NaN' + ',' + Jsc[1] + ',' + Voc[1] + ',' + FF[1] + ',' + MPP[1] + ',' + 'NaN' + ',' + EPCE[1] + '\n')    
+
+    
+for i in range (len(ListaArchivos)):   #para todos los archivos donde se encuentre el programa
+
+    readfiles(DirectorioActual+"\\"+ListaArchivos[i],ListaArchivos[i])
